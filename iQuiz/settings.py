a89eb8ae72da84098270d2e396fb6a70b8ff1469
Aravidhan <aravindhan.ct19@bitsathy.ac.in>
2021-08-09 17:17:07 +0530

    add django-heroku and postgres

--- iQuiz/settings.py
+++ iQuiz/settings.py
@@ -12,9 +12,9 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 
 import os
 from .iQuiz_settings import *   # import the LTI_OAUTH_CREDENTIALS
-# import django_heroku
+import django_heroku
 
-# django_heroku.settings(locals)
+django_heroku.settings(locals)
 
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -88,12 +88,14 @@ WSGI_APPLICATION = 'iQuiz.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
+
 
 
 # Password validation
