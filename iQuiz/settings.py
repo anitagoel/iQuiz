b9e71dc577ea66022a90e08d3a055365642ab890
Aravidhan <aravindhan.ct19@bitsathy.ac.in>
2021-08-09 17:25:50 +0530

    removed django_heroku

--- iQuiz/settings.py
+++ iQuiz/settings.py
@@ -12,9 +12,9 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 
 import os
 from .iQuiz_settings import *   # import the LTI_OAUTH_CREDENTIALS
-import django_heroku, dj_database_url
+import dj_database_url
 
-django_heroku.settings(locals)
+# django_heroku.settings(locals)
 
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
