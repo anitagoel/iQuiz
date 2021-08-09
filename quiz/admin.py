0ecfb7bd2d56be48dae866de25645522e5ddaf0b
Aravidhan <aravindhan.ct19@bitsathy.ac.in>
2021-08-09 19:32:59 +0530

    added models to admin

--- quiz/admin.py
+++ quiz/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
 from .models import Quiz, Question, QuizSettings
+from .models.answer import Answer
+from .models.lti_user import LTIUser
+from .models.response import Response
 from django.contrib.admin.models import LogEntry
 admin.site.register(LogEntry)
 
@@ -7,3 +10,6 @@ admin.site.register(LogEntry)
 admin.site.register(Quiz)
 admin.site.register(QuizSettings)
 admin.site.register(Question)
+admin.site.register(Answer)
+admin.site.register(LTIUser)
+admin.site.register(Response)
