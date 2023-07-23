bfe471ccd65cf96ece2ea9f260306df42a86c94a
Aravindhan A <aravindhan.asaithambi@cdw.com>
2023-07-23 21:44:20 +0530

    Removed unused import

--- iQuiz/urls.py
+++ iQuiz/urls.py
@@ -15,7 +15,6 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from django.conf.urls import url
 
 urlpatterns = [
     path('admin/', admin.site.urls),
